fab19a986510a2d89b7611861683157b3a3beb9c
Seif Mohamed <seifeldinh@banquemisr.com>
2022-11-14 13:28:51 +0200

    Add RepositoryTest to AppTestSuite

--- 05-unit-tests/projects/starter/app/src/test/java/com/raywenderlich/android/cocktails/game/model/AppTestSuite.kt
+++ 05-unit-tests/projects/starter/app/src/test/java/com/raywenderlich/android/cocktails/game/model/AppTestSuite.kt
@@ -1,9 +1,16 @@
 package com.raywenderlich.android.cocktails.game.model
 
 import com.raywenderlich.android.cocktails.game.factory.CocktailsGameFactoryTest
+import com.raywenderlich.android.cocktails.game.repository.RepositoryTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
-@Suite.SuiteClasses(GameTest::class, QuestionTest::class, ScoreTest::class, CocktailsGameFactoryTest::class)
+@Suite.SuiteClasses(
+    GameTest::class,
+    QuestionTest::class,
+    ScoreTest::class,
+    CocktailsGameFactoryTest::class,
+    RepositoryTest::class
+)
 class AppTestSuite
