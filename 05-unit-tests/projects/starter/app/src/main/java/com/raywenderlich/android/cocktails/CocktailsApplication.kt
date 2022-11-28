3591da2e17f8bb2420e7d7762ae1321c3af666b4
Seif Mohamed <seifeldinh@banquemisr.com>
2022-11-28 18:06:26 +0200

    - update CocktailsApplication.kt - update CocktailsGameActivity.kt

--- 05-unit-tests/projects/starter/app/src/main/java/com/raywenderlich/android/cocktails/CocktailsApplication.kt
+++ 05-unit-tests/projects/starter/app/src/main/java/com/raywenderlich/android/cocktails/CocktailsApplication.kt
@@ -31,9 +31,22 @@
 package com.raywenderlich.android.cocktails
 
 import android.app.Application
+import android.content.Context
+import com.raywenderlich.android.cocktails.common.network.CocktailsApi
+import com.raywenderlich.android.cocktails.common.repository.Repository
+import com.raywenderlich.android.cocktails.common.repository.RepositoryImpl
+import com.raywenderlich.android.cocktails.game.factory.CocktailsGameFactory
+import com.raywenderlich.android.cocktails.game.factory.CocktailsGameFactoryImpl
 
 class CocktailsApplication : Application() {
-  // TODO: Repository
+    val repository: Repository by lazy {
+        RepositoryImpl(
+            CocktailsApi.create(),
+            getSharedPreferences("Cocktails", Context.MODE_PRIVATE)
+        )
+    }
 
-  // TODO: Game factory
+    val gameFactory: CocktailsGameFactory by lazy {
+        CocktailsGameFactoryImpl(repository)
+    }
 }
--- 05-unit-tests/projects/starter/app/src/main/java/com/raywenderlich/android/cocktails/CocktailsGameActivity.kt
+++ 05-unit-tests/projects/starter/app/src/main/java/com/raywenderlich/android/cocktails/CocktailsGameActivity.kt
@@ -31,16 +31,132 @@
 package com.raywenderlich.android.cocktails
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.raywenderlich.android.cocktails.databinding.ActivityGameBinding
+import com.raywenderlich.android.cocktails.game.model.Question
+import com.raywenderlich.android.cocktails.game.model.Score
+import com.raywenderlich.android.cocktails.game.viewmodel.CocktailsGameViewModel
+import com.raywenderlich.android.cocktails.game.viewmodel.CocktailsGameViewModelFactory
 
 class CocktailsGameActivity : AppCompatActivity() {
 
-  private lateinit var binding: ActivityGameBinding
+    private lateinit var binding: ActivityGameBinding
+    private lateinit var cocktailsGameViewModel: CocktailsGameViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    binding = ActivityGameBinding.inflate(layoutInflater)
-    setContentView(binding.root)
-  }
+        val repository = (application as CocktailsApplication).repository
+        val factory = (application as CocktailsApplication).gameFactory
+        cocktailsGameViewModel =
+            ViewModelProvider(this, CocktailsGameViewModelFactory(repository, factory))
+                .get(CocktailsGameViewModel::class.java)
+
+        cocktailsGameViewModel.getLoading().observe(this) {
+            it?.let { loading -> showLoading(loading) }
+        }
+        cocktailsGameViewModel.getError().observe(this) {
+            it?.let { error -> showError(error) }
+        }
+        cocktailsGameViewModel.getQuestion().observe(this) {
+            showQuestion(it)
+        }
+        cocktailsGameViewModel.getScore().observe(this) {
+            it?.let { score -> showScore(score) }
+        }
+        cocktailsGameViewModel.finishGame.observe(this) {
+            when (it) {
+                true -> {
+                    binding.nextButton.isEnabled = false
+                    binding.tvGameFinished.visibility = View.VISIBLE
+                }
+                false -> {
+                    binding.nextButton.isEnabled = true
+                    binding.tvGameFinished.visibility = View.GONE
+                }
+            }
+        }
+
+        binding.nextButton.setOnClickListener { cocktailsGameViewModel.nextQuestion() }
+
+        cocktailsGameViewModel.initGame()
+    }
+
+    private fun showScore(score: Score) {
+        binding.scoreTextView.text = getString(R.string.game_score, score.current)
+        binding.highScoreTextView.text = getString(R.string.game_highscore, score.highest)
+    }
+
+    private fun showQuestion(question: Question?) {
+        if (question != null) {
+            binding.mainGroup.visibility = View.VISIBLE
+
+            if (question.answerOption != null) {
+                showAnsweredQuestion(question)
+            } else {
+                showUnansweredQuestion(question)
+            }
+        } else {
+            binding.mainGroup.visibility = View.GONE
+            binding.noResultsTextView.visibility = View.VISIBLE
+            binding.questionResultImageView.visibility = View.GONE
+        }
+    }
+
+    private fun showUnansweredQuestion(question: Question) {
+        val options = question.getOptions()
+        binding.firstOptionButton.text = options[0]
+        binding.firstOptionButton.setOnClickListener {
+            cocktailsGameViewModel.answerQuestion(
+                question,
+                binding.firstOptionButton.text.toString()
+            )
+        }
+        binding.firstOptionButton.isEnabled = true
+        binding.secondOptionButton.text = options[1]
+        binding.secondOptionButton.setOnClickListener {
+            cocktailsGameViewModel.answerQuestion(
+                question,
+                binding.secondOptionButton.text.toString()
+            )
+        }
+        binding.secondOptionButton.isEnabled = true
+
+        Glide.with(binding.cocktailImageView).load(question.imageUrl)
+            .into(binding.cocktailImageView)
+        binding.questionResultImageView.visibility = View.GONE
+    }
+
+    private fun showAnsweredQuestion(question: Question) {
+        binding.firstOptionButton.setOnClickListener(null)
+        binding.firstOptionButton.isEnabled = false
+        binding.secondOptionButton.setOnClickListener(null)
+        binding.secondOptionButton.isEnabled = false
+        if (question.isAnsweredCorrectly) {
+            binding.questionResultImageView.setImageResource(R.drawable.ic_check_24dp)
+        } else {
+            binding.questionResultImageView.setImageResource(R.drawable.ic_error_24dp)
+        }
+        binding.questionResultImageView.visibility = View.VISIBLE
+    }
+
+    private fun showError(show: Boolean) {
+        if (show) {
+            binding.errorContainer.root.visibility = View.VISIBLE
+            binding.errorContainer.retryButton.setOnClickListener { cocktailsGameViewModel.initGame() }
+        } else {
+            binding.errorContainer.root.visibility = View.GONE
+        }
+    }
+
+    private fun showLoading(show: Boolean) {
+        binding.loadingContainer.root.visibility = if (show) View.VISIBLE else View.GONE
+        binding.mainGroup.visibility = View.GONE
+        binding.noResultsTextView.visibility = View.GONE
+        binding.questionResultImageView.visibility = View.GONE
+    }
 }
